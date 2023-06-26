839af5570e1b13fbe2b4ab1590ad198b36bd68b4
saeid4x <saeid.almahdi@gmail.com>
2023-06-26 05:42:28 +0430

    edit readme-md file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RealEstate_react-en
+
+<hr>
+![automotive-car-design-en](./src/assets/img/final/2.jpg) <br /> <br />
+![automotive-car-design-en](./src/assets/img/final/1.jpg)
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ import Search from "../components/Search";
 const Banner = () => {
   return (
     <section className="h-full max-h-[640px] mb-8 xl:mb-24">
-      <div className="flex flex-col lg:flex-row border-4 border-indigo-600">
+      <div className="flex flex-col lg:flex-row border-4 ">
         <div
           className="lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start
         text-center lg:text-left justify-center  flex-1 px-4 lg:px-0"
